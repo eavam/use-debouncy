81675ea2a55b01bb986256baf3cb14cbf9ee0a36
Egor Avakumov <avakymov21@gmail.com>
2021-01-24 20:01:06 +0700

    test: migration on cypress

--- jest.e2e.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'e2e',
-  preset: 'jest-playwright-preset',
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/e2e/browser.test.ts'],
-};
--- jest.unit.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'unit',
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/index.test.ts'],
-};
