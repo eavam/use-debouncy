0708fd364434bdbfc64dc40ec6bc2c444bb9d10f
Egor Avakumov <avakymov21@gmail.com>
2021-02-23 23:39:58 +0700

    test: update e2e tests with new functions

--- cypress/app/index.tsx
+++ cypress/app/index.tsx
@@ -1,42 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import useDebouncy from '../../lib/effect';
+import { SearchPeoplesWithEffect } from './src/searchPeoplesWithEffect';
+import { SearchPeoplesWithFn } from './src/searchPeoplesWithFn';
 
 const App = () => {
-  const [value, setValue] = useState('');
-  const [peoples, setPeoples] = useState([]);
-
-  const fetchPeoples = async (search: string) => {
-    try {
-      const searchedPeoples = await fetch(
-        `https://swapi.dev/api/people/?search=${search}`,
-      );
-
-      const data = await searchedPeoples.json();
-      setPeoples(data.results);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useDebouncy(
-    () => {
-      fetchPeoples(value);
-    },
-    400,
-    [value],
-  );
-
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>): void =>
-    setValue(event.target.value);
-
   return (
-    <div>
-      <input data-qa="input/search" value={value} onChange={onChange} />
-      {peoples.map(({ name }) => (
-        <div key={name}>{name}</div>
-      ))}
-    </div>
+    <>
+      <SearchPeoplesWithEffect />
+      <div style={{ height: 30 }} />
+      <SearchPeoplesWithFn />
+    </>
   );
 };
 
---
+++ cypress/app/src/usePeoples.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+interface UsePeoplesHook {
+  (): {
+    fetchPeoples: (search: string) => Promise<void>;
+    peoples: { name: string }[];
+  };
+}
+
+export const usePeoples: UsePeoplesHook = () => {
+  const [peoples, setPeoples] = useState([]);
+
+  const fetchPeoples = async (search: string) => {
+    try {
+      const searchedPeoples = await fetch(
+        `https://swapi.dev/api/people/?search=${search}`,
+      );
+
+      const data = await searchedPeoples.json();
+      setPeoples(data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return { fetchPeoples, peoples };
+};
