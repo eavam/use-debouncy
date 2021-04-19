3f9f784feaed9e2bc510967ea7cfac735efce437
Egor Avakumov <avakymov21@gmail.com>
2021-04-20 00:29:30 +0700

    revert: this reverts commit 5952cffe6d1333c14836e2e033681af703bdfc86

--- src/core.ts
+++ src/core.ts
@@ -8,6 +8,9 @@ interface RenderFrameFn {
   (cb: BlankFn): FrameRequestCallback;
 }
 
+// 16 ms its time on 1 frame
+const FRAME_MS = 16;
+
 export const useAnimationFrame = <T>(
   callback: (...args: T[]) => void,
   wait: number,
@@ -17,6 +20,10 @@ export const useAnimationFrame = <T>(
 
   const renderFrame = useCallback<RenderFrameFn>(
     (cb) => (timeNow) => {
+      //Call will be after the first frame.
+      // Requires subtracting 16 ms for more accurate timing.
+      timeStart.current = timeStart.current || timeNow - FRAME_MS;
+
       // Call next rAF if time is not up
       if (timeNow - timeStart.current < wait) {
         rafId.current = requestAnimationFrame(renderFrame(cb));
@@ -34,7 +41,7 @@ export const useAnimationFrame = <T>(
   return useCallback(
     (...args: T[]) => {
       // Reset timer and previous animation before new animation frame
-      timeStart.current = Date.now();
+      timeStart.current = 0;
       cancelAnimationFrame(rafId.current);
 
       rafId.current = requestAnimationFrame(
