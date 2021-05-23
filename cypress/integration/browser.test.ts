48bdfc8a2b4b86849ed4cf702c7087dfcad46865
Egor Avakumov <avakymov21@gmail.com>
2021-05-24 01:57:36 +0700

    fix: giving up date.now (#496)

--- cypress/integration/browser.test.ts
+++ cypress/integration/browser.test.ts
@@ -5,7 +5,6 @@ describe('useDebouncy', () => {
   for (const input of ['effect', 'fn']) {
     it(`input with ${input}`, () => {
       const inputSelector = getInputSelector(input);
-      cy.clock();
       cy.visit('/');
       cy.intercept(
         {
@@ -16,15 +15,13 @@ describe('useDebouncy', () => {
       ).as('swapi');
 
       cy.get(inputSelector).type('Dar');
-      cy.tick(1001);
-      cy.wait('@swapi');
+      cy.wait('@swapi', { timeout: 10000 });
       cy.get('@swapi.all').should('have.length', 1);
 
       cy.get(inputSelector)
         .type('{selectall} Dart')
         .type(' Vader', { timeout: 300 });
-      cy.tick(1001);
-      cy.wait('@swapi').then((interception) => {
+      cy.wait('@swapi', { timeout: 10000 }).then((interception) => {
         assert.include(
           interception.request.url,
           encodeURIComponent('Dart Vader'),
@@ -33,8 +30,7 @@ describe('useDebouncy', () => {
       cy.get('@swapi.all').should('have.length', 2);
 
       cy.get(inputSelector).clear();
-      cy.tick(1001);
-      cy.wait('@swapi').then((interception) => {
+      cy.wait('@swapi', { timeout: 10000 }).then((interception) => {
         assert.match(interception.request.url, /search=$/);
       });
       cy.get('@swapi.all').should('have.length', 3);
